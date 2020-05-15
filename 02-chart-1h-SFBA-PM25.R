c91c9881ce29c1486a1e9794a7a035820fb51b06
David Holstius <david.holstius@gmail.com>
2020-05-15 14:43:34 -0700

    extract charting code into function

--- 02-chart-1h-SFBA-PM25.R
+++ 02-chart-1h-SFBA-PM25.R
@@ -1,81 +1,5 @@
 source(here::here("code", "tidy_1h_data.R"))
 
-#'
-#' TODO: Expand `value_vars` beyond just "PM25".
-#' 
-chart_vars <- 
-  tidyselect::vars_select(
-    names(SFBA_1h_data),
-    PM25)
-
-#'
-#' This chart begins at January 1, 2020.
-#' 
-chart_data <-
-  SFBA_1h_data %>%
-  tidy_1h_data(
-    value_vars = chart_vars,
-    na.rm = TRUE) %>%
-  with_epoch(
-    na.rm = TRUE) %>%
-  filter(
-    dttm >= ISOdate(2020, 01, 01, hour = 00, tz = dttm_tz))
-
-#'
-#' We'll use ths in `coord_cartesian(ylim = .)` later on, instead
-#' of `scale_y_continous(limits = ., ...)`. That will ensure that,
-#' even though we are clipping the viewport, we aren't dropping the
-#' data --- so things like `geom_smooth()` will be using the full
-#' `chart_data` as the basis for smoothing.
-#'
-chart_y_limits <- c(
-  5 * floor(min(0, with(chart_data, min(value, na.rm = TRUE))) / 5),
-  35)
-
-#'
-#' TODO: improve chart title and subtitle.
-#'
-chart_description <- 
-  ggplot2::labs(
-    title = "Bay Area Monitoring Data (PM2.5, Hourly)",
-    subtitle = glue(
-      "Source: AirNowTech.",
-      "Points jittered to reduce overplotting.",
-      "Y-axis clipped at {max(chart_y_limits)}, but no data are dropped when calculating group means.",
-      .sep = "\n"),
-    caption = glue("DRAFT {format(Sys.Date(), '%Y-%m-%d')}"))
-
-#'
-#' Let's show `dttm_tz` (the timezone) on the x-axis.
-#'
-chart_x_axis_title <-
-  stringr::str_remove(
-    lubridate::tz(pull(chart_data, dttm)),
-    fixed("Etc/"))
-
-#'
-#' TODO: show vertical lines on Sundays instead of Mondays?
-#'
-chart_x_scale <-
-  scale_x_datetime(
-    name = chart_x_axis_title,
-    expand = expansion(0, 0),
-    date_labels = "%d\n%b",
-    date_breaks = "2 weeks",
-    date_minor_breaks = "1 week")
-
-chart_y_scale <-
-  scale_y_continuous(
-    name = NULL,
-    expand = expansion(mult = 0, add = 0))
-
-chart_faceting <-
-  lemon::facet_rep_wrap(
-    ~ SiteName, 
-    #scales = "free_y",
-    repeat.tick.labels = "y",
-    ncol = 3)
-
 chart_guides <-
   guides(
     color = guide_legend(
@@ -91,40 +15,134 @@ chart_theme <-
     #axis.text.y = element_text(vjust = 0),
     plot.title = element_text(face = "bold"))
 
-chart_object <-
-  ggplot(chart_data) +
-  aes(x = dttm, y = value, group = SiteName) +
-  aes(color = epoch) +
-  geom_hline(
-    size = I(0.5),
-    yintercept = 0) +
-  geom_point(
-    position = position_jitter(height = 0.5),
-    show.legend = FALSE,
-    size = I(0.3),
-    alpha = I(0.2)) +
-  ggthemes::scale_color_excel_new() +
-  chart_faceting +
-  chart_x_scale +
-  chart_y_scale +
-  chart_guides +
-  chart_theme +
-  chart_description +
-  geom_smooth(
-    aes(group = str_c(epoch, SiteName)),
-    method = "lm",
-    se = FALSE,
-    size = I(0.75),
-    show.legend = FALSE,
-    formula = y ~ x + 0) +
-  coord_cartesian(
-    ylim = chart_y_limits,
-    clip = FALSE) # don't actually drop any data, as `limits` would do
+chart_color_scale <-
+  ggthemes::scale_color_excel_new()
 
-show(chart_object)
+make_1h_stripchart <- function (
+  input_data,
+  value_var,
+  value_limits,
+  ...
+) {
+  
+  chart_data <-
+    input_data %>%
+    tidy_1h_data(
+      value_vars = value_var,
+      na.rm = TRUE) %>%
+    with_epoch(
+      na.rm = TRUE)
+  
+  #'
+  #' NOTE: `value_limits` is used in `coord_cartesian(ylim = .)`, instead
+  #' of `scale_y_continous(limits = ., ...)`. That will ensure that,
+  #' even though we are clipping the viewport, we aren't dropping the
+  #' data --- so things like `geom_smooth()` will be using the full
+  #' `chart_data` as the basis for smoothing.
+  #'
+  
+  #'
+  #' Let's show `dttm_tz` (the timezone) on the x-axis.
+  #'
+  chart_x_axis_title <-
+    stringr::str_remove(
+      lubridate::tz(pull(chart_data, dttm)),
+      fixed("Etc/"))
+  
+  #'
+  #' TODO: show vertical lines on Sundays instead of Mondays?
+  #'
+  chart_x_scale <-
+    scale_x_datetime(
+      name = chart_x_axis_title,
+      expand = expansion(0, 0),
+      date_labels = "%d\n%b",
+      date_breaks = "2 weeks",
+      date_minor_breaks = "1 week")
+  
+  chart_y_scale <-
+    scale_y_continuous(
+      name = NULL,
+      expand = expansion(mult = 0, add = 0))
+  
+  chart_faceting <-
+    lemon::facet_rep_wrap(
+      ~ SiteName, 
+      repeat.tick.labels = "y",
+      ncol = 3)
+  
+  chart_object <-
+    ggplot(chart_data) +
+    aes(x = dttm, y = value, group = SiteName) +
+    aes(color = epoch) +
+    geom_hline(
+      size = I(0.5),
+      yintercept = 0) +
+    geom_point(
+      position = position_jitter(height = 0.5),
+      show.legend = FALSE,
+      size = I(0.3),
+      alpha = I(0.2)) +
+    chart_color_scale +
+    chart_faceting +
+    chart_x_scale +
+    chart_y_scale +
+    chart_guides +
+    chart_theme +
+    chart_description +
+    geom_smooth(
+      aes(group = str_c(epoch, SiteName)),
+      method = "lm",
+      se = FALSE,
+      size = I(0.75),
+      show.legend = FALSE,
+      formula = y ~ x + 0) +
+    coord_cartesian(
+      ylim = value_limits,
+      clip = FALSE) # don't actually drop any data, as `limits` would do
+  
+  return(chart_object)
+  
+}
+
+#'
+#' This chart begins at January 1, 2020.
+#' 
+SFBA_PM25_1h_stripchart <- local({
+  
+  #'
+  #' TODO: improve chart title and subtitle.
+  #'
+  chart_description <- 
+    ggplot2::labs(
+      title = "Ambient PM2.5 Measurements",
+      subtitle = glue::glue(
+        "Source: AirNowTech.",
+        "Points jittered to reduce overplotting.",
+        "Y-axis clipped at {max(chart_y_limits)}, but no data are dropped when calculating group means.",
+        .sep = "\n"),
+      caption = glue::glue(
+        "DRAFT {format(Sys.Date(), '%Y-%m-%d')}"))
+  
+  filtered_data <-
+    SFBA_1h_data %>%
+    filter(
+      dttm >= ISOdate(2020, 01, 01, hour = 00, tz = dttm_tz))
+    
+  SFBA_PM25_1h_stripchart <-
+    make_1h_stripchart(
+      filtered_data,
+      value_var = "PM25",
+      value_unit = "ug/m3",
+      value_limits = c(-5, 35)) +
+    chart_description
+  
+})
+
+show(SFBA_PM25_1h_stripchart)
 
 ggplot2::ggsave(
-  here::here("figures", "chart-1h-SFBA-PM25.pdf"),
-  chart_object,
+  here::here("figures", "SFBA-PM25-1h-stripchart.pdf"),
+  SFBA_PM25_1h_stripchart,
   width = 11,
   height = 17)
