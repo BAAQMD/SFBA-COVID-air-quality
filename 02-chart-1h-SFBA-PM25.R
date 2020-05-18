c9adc82a15e749bb1450ba896ea4384daf0fd6eb
David Holstius <david.holstius@gmail.com>
2020-05-18 09:27:59 -0700

    lift out general chart theme, guides, and color scale

--- 02-chart-1h-SFBA-PM25.R
+++ 02-chart-1h-SFBA-PM25.R
@@ -1,22 +1,5 @@
 source(here::here("code", "tidy_1h_data.R"))
-
-chart_guides <-
-  guides(
-    color = guide_legend(
-      title = "Epoch",
-      override.aes = list(size = 3, alpha = 1)))
-
-chart_theme <-
-  theme_minimal() +
-  theme(
-    axis.title.x = element_text(size = rel(0.9), margin = margin(1.5, 0, 0, 0, "lines")),
-    strip.text = element_text(size = rel(0.9), hjust = 0, face = "bold"),
-    panel.spacing.x = unit(2, "lines"),
-    #axis.text.y = element_text(vjust = 0),
-    plot.title = element_text(face = "bold"))
-
-chart_color_scale <-
-  ggthemes::scale_color_excel_new()
+source(here::here("code", "charts.R"))
 
 make_1h_stripchart <- function (
   input_data,
@@ -32,6 +15,8 @@ make_1h_stripchart <- function (
       na.rm = TRUE) %>%
     with_epoch(
       na.rm = TRUE)
+  
+  chart_data %>% pull(SiteName) %>% unique() %>% print()
   
   #'
   #' NOTE: `value_limits` is used in `coord_cartesian(ylim = .)`, instead
---
+++ code/charts.R
@@ -0,0 +1,17 @@
+chart_guides <-
+  guides(
+    color = guide_legend(
+      title = "Epoch",
+      override.aes = list(size = 3, alpha = 1)))
+
+chart_theme <-
+  theme_minimal() +
+  theme(
+    axis.title.x = element_text(size = rel(0.9), margin = margin(1.5, 0, 0, 0, "lines")),
+    strip.text = element_text(size = rel(0.9), hjust = 0, face = "bold"),
+    panel.spacing.x = unit(2, "lines"),
+    #axis.text.y = element_text(vjust = 0),
+    plot.title = element_text(face = "bold"))
+
+chart_color_scale <-
+  ggthemes::scale_color_excel_new()
