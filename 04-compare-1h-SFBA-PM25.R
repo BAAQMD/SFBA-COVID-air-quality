2505f98a9667fd410e87bbf951cab9f48fcc69a6
David Holstius <david.holstius@gmail.com>
2020-05-18 11:18:46 -0700

    show t statistic in addition to D

--- 04-compare-1h-SFBA-PM25.R
+++ 04-compare-1h-SFBA-PM25.R
@@ -1,3 +1,78 @@
+source(here::here("code", "chart-helpers.R"))
+source(here::here("code", "tidy_1h_data.R"))
+source(here::here("code", "with_epoch.R"))
+
+#'
+#' To apply a hypothesis test to `Pre` and `Post` from `input_data`,
+#' where our `input_data` is already in tidy form (i.e., having columns 
+#' `epoch`, `variable`, and `value`).
+#' 
+#' Returns a tidied result (see `broom::tidy()`).
+#'
+tidy_htest <- function (
+  input_data,
+  htest,
+  ...
+) {
+  if (rlang::is_empty(input_data)) return(NULL)
+  data_list <- split(input_data, input_data$epoch)
+  value_list <- map(data_list, pull, value) 
+  quieted <- purrr::invoke(quietly(htest), unname(value_list))
+  result <- pluck(quieted, "result")
+  tidied <- broom::tidy(result)
+  return(tidied)
+}
+
+#'
+#' Summarize pre-vs-post differences in the PM2.5 data with a few 
+#' convenient statistics (i.e., hypothesis tests).
+#' 
+#' TODO: make the statistics more pluggable.
+#' 
+#' FIXME: `ks.test()` is for *continuous* distributions. These ECDFs
+#' are of rounded measurements --- they have been discretized. See:
+#' 
+#' 
+#'
+make_htest_data <- function (
+  input_data,
+  value_vars = c("PM25", "NO2")
+) {
+  
+  nested_data <-
+    input_data %>%
+    group_by(
+      SiteName,
+      variable) %>%
+    nest() 
+  
+  summarised_data <-
+    nested_data %>%
+    summarise_at(
+      vars(data),
+      list(
+        ks.test = ~ map(., tidy_htest, ks.test),
+        t.test = ~ map(., tidy_htest, t.test))) %>%
+    ungroup() 
+  
+  tidied_data <-
+    summarised_data %>%
+    gather(
+      stat_fun,
+      stat_obj,
+      dplyr::matches(".test")) %>%
+    mutate_at(
+      vars(stat_obj),
+      ~ map(., select, stat_value = statistic, stat_p_value = p.value)) %>%
+    unnest(
+      c(stat_obj)) %>%
+    mutate(
+      stat_var = recode(stat_fun, ks.test = "D", t.test = "t")) 
+  
+  return(tidied_data)
+  
+}
+
 #'
 #' To make a chart of ECDFs, faceted by site, for a given `value_var`.
 #'
@@ -35,14 +110,46 @@ make_ecdf_chart <- function (
       chart_x_title,
       expand = expansion(0, 0))
   
+  format_stat <- function (stat_var, stat_value, stat_p_value) {
+    stars <- if_else(stat_p_value < 0.05, "**", "  ")
+    glue::glue(
+      "{stat_var} = {format_decimal(stat_value, digits = 2)}{stars}")
+  }
+  
+  label_layer <- local({
+    
+    label_data <-  
+      chart_data %>%
+      make_htest_data(
+        value_vars = value_var) %>%
+      group_by(
+        SiteName) %>%
+      summarise(
+        stat_text = glue::glue_collapse(
+          format_stat(stat_var, stat_value, stat_p_value),
+          sep = "\n"))
+    
+    geom_text(
+      aes(label = stat_text, x = 35, y = 0),
+      vjust = -0.1,
+      hjust = 1,
+      size = I(3.0),
+      data = label_data)
+    
+  })
+  
+  ecdf_layer <-
+    geom_step(
+      aes(color = epoch),
+      show.legend = FALSE,
+      stat = "ecdf") 
+  
   chart_object <-
     chart_data %>%
     ggplot() +
     aes(x = value) +
-    geom_step(
-      aes(color = epoch),
-      show.legend = FALSE,
-      stat = "ecdf") +
+    ecdf_layer +
+    label_layer + 
     chart_color_scale +
     chart_faceting +
     chart_guides +
@@ -70,8 +177,8 @@ SFBA_1h_PM25_ecdf_chart <- local({
       title = "Ambient PM2.5 Measurements",
       subtitle = str_c(
         "Shown here are empirical cumulative distributions for PM2.5, \"pre\" and \"post\".",
-        "The Kolmogorov D statistic quantifies the area between two such curves.",
-        "Larger D values correspond to larger differences.",
+        "The Kolmogorov D statistic quantifies the area between two such curves (larger = more different).",
+        "The t statistic is also shown. Both are two-sided, and do not assume equal variance.",
         sep = "\n"),
       caption = glue::glue(
         "DRAFT {format(Sys.Date(), '%Y-%m-%d')}"))
@@ -92,77 +199,6 @@ SFBA_1h_PM25_ecdf_chart <- local({
 })
 
 show(SFBA_1h_PM25_ecdf_chart)
-
-#' 
-#' Nest the data. This should be helpful for summarizing it.
-#' 
-#' TODO: use the `broom` package here.
-#' 
-nested_SFBA_1h_PM25_data <-
-  SFBA_1h_data %>%
-  tidy_1h_data(
-    value_vars = "PM25") %>%
-  with_epoch() %>%
-  group_by(
-    SiteName,
-    variable) %>%
-  nest() %>%
-  spread(
-    variable,
-    data) %>%
-  ungroup() %>%
-  glimpse()
-
-#'
-#' To extract a two-element list (`Pre` and `Post`) from `input_data`,
-#' where our `input_data` is already in tidy form (i.e., having columns 
-#' `epoch`, `variable`, and `value`).
-#'
-pre_vs_post <- function (
-  input_data,
-  stat,
-  ...
-) {
-  if (rlang::is_empty(input_data)) return(NULL)
-  data_list <- split(input_data, input_data$epoch)
-  value_list <- map(data_list, pull, value) 
-  obj <- purrr::invoke(quietly(stat), unname(value_list))
-  result <- pluck(obj, "result")
-  return(result)
-}
-
-#'
-#' Summarize pre-vs-post differences in the PM2.5 data with a few 
-#' convenient statistics (i.e., hypothesis tests).
-#'
-SFBA_1h_PM25_summary_data <-
-  nested_SFBA_1h_PM25_data %>%
-  select(
-    SiteName,
-    PM25) %>%
-  mutate(
-    D_htest = map(PM25, pre_vs_post, stat = ks.test),
-    t_htest = map(PM25, pre_vs_post, stat = t.test),
-    D = map_dbl(D_htest, pluck, "statistic", .default = NA_real_)) %>%
-  filter_at(
-    vars(D),
-    any_vars(is.finite(.)))
-
-#'
-#' Put the D-statistic onto our ECDF chart.
-#' 
-#' TODO: put a 95% CI for a two-sample t-test on here too.
-#' 
-SFBA_1h_PM25_ecdf_chart_v2 <-
-  SFBA_1h_PM25_ecdf_chart +
-  geom_text(
-    aes(label = str_c("D = ", format_decimal(D, digits = 2)), x = 35, y = 0),
-    vjust = -0.1,
-    hjust = 1,
-    size = I(3.5),
-    data = PM25_summary_data)
-
-show(SFBA_1h_PM25_ecdf_chart_v2)
 
 ggplot2::ggsave(
   here::here("figures", "SFBA-PM25-1h-ecdf-chart.pdf"),
